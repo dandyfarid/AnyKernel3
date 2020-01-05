b30cdac279d77ed72a4c77e9509db50a80554d44
dandyfarid <yuukoito98@gmail.com>
2020-01-05 13:46:02 +0700

    MonnlightMelody
    Change-Id: I49a3d1e6b3c781e91c89b72a2715a72f74e4a8c7

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=hectopascal by dandyfarid @ xda-developers
+kernel.string=MoonlightMelody by dandyfarid @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
