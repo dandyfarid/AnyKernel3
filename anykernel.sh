8581c840dc92f24d0587f1cf9dc3c8fbb84906b6
koito97 <yuukoito98@gmail.com>
2020-01-23 22:12:07 +0700

    StarDiamond !!
    Change-Id: I5da6ba77930038f252918a5bc38423e11e704fcc

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=MoonlightMelody by dandyfarid @ xda-developers
+kernel.string=StarDiamond by dandyfarid @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
